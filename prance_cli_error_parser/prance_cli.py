0a68c10ac31e07fcdc9f24c31608766a02c3fb26
Gilad Barnea <cr-gbarn-herolo@allot.com>
2021-09-15 17:35:49 +0300

    --max-depth support

--- prance_cli_error_parser/prance_cli.py
+++ prance_cli_error_parser/prance_cli.py
@@ -8,11 +8,73 @@ from prance import ResolvingParser
 
 from prance_cli_error_parser.util import pretty_type, shorten
 
+argparser = ArgumentParser()
+argparser.add_argument(
+        "target",
+        type=str,
+        help='openapi.yaml file path',
+        )
+argparser.add_argument(
+        "--rows",
+        type=int,
+        default=1,
+        help='Maximum rows per exception argument (a shortened string is displayed). Default 1'
+        )
+argparser.add_argument(
+        "--width",
+        type=int,
+        help="Defaults to active terminal's width (run 'echo $COLUMNS' to view)"
+        )
+argparser.add_argument(
+        "--max-depth",
+        type=int,
+        help="No depth limit by default"
+        )
+argparser.add_argument(
+        "--backend",
+        type=str,
+        choices=['openapi-spec-validator', ],
+        default='openapi-spec-validator',
+        help='Default: openapi-spec-validator'
+        )
+argparser.add_argument(
+        "--working-dir",
+        type=str,
+        default='.',
+        help='Set script working directory'
+        )
+
+parsed_args = argparser.parse_args()
+target = parsed_args.target
+rows = parsed_args.rows
+backend = parsed_args.backend
+working_dir = parsed_args.working_dir
+width = parsed_args.width
+max_depth = parsed_args.max_depth
+
+os.chdir(working_dir)
+
+if not width:
+    width = 10 * (int(os.get_terminal_size()[0]) // 10) - 5  # round down
+
+if not max_depth:
+    from math import inf
+    max_depth = inf
 indent = -1
 
 
-def print_error_args(args: Iterable, *, width, rows):
+def format_primitive(arg, *, idx, indent_size, prefix, tabs):
+    short = shorten(str(arg), limit=(width - indent_size) * rows)
+    wrapped = textwrap.fill(short, width=width - indent_size + prefix)
+    joiner = f'\n{tabs}' + ' ' * prefix
+    formatted = f'{idx} ' + joiner.join(wrapped.splitlines())
+    return formatted
+
+
+def print_error_args(args: Iterable):
     global indent
+    if indent == max_depth:
+        return
     indent += 1
     tabs = '\033[38;2;40;40;40m' + '· ' * indent + '\033[0m'
     for i, arg in enumerate(args):
@@ -23,70 +85,21 @@ def print_error_args(args: Iterable, *, width, rows):
         idx = f'{tabs}\x1b[90m[{i}]\x1b[0m'
         typ = f'\x1b[90m{pretty_type(type(arg))}\x1b[0m'
         if isinstance(arg, Mapping):
-            # not implemented
-            print(idx, typ)
-            print(arg)
+            formatted = format_primitive(arg, idx=idx, indent_size=indent_size, prefix=prefix, tabs=tabs)
+            print(formatted)
         elif isinstance(arg, Iterable) and not isinstance(arg, str):
             print(idx, typ)
-            print_error_args(arg, width=width, rows=rows)
+            print_error_args(arg)
         elif isinstance(arg, BaseException):
             print(idx, typ)
-            print_error_args(arg.args, width=width, rows=rows)
+            print_error_args(arg.args)
         else:
-            actual_width = width - indent_size
-            short = shorten(str(arg), limit=actual_width * rows)
-            wrapped = textwrap.fill(short, width=actual_width + prefix)
-            joiner = f'\n{tabs}' + ' ' * prefix
-            print(f'{idx} ' + joiner.join(wrapped.splitlines()))
+            formatted = format_primitive(arg, idx=idx, indent_size=indent_size, prefix=prefix, tabs=tabs)
+            print(formatted)
     else:
         indent -= 1
 
-
 def main():
-    argparser = ArgumentParser()
-    argparser.add_argument(
-            "target",
-            type=str,
-            help='openapi.yaml file path',
-            )
-    argparser.add_argument(
-            "--rows",
-            dest="rows",
-            type=int,
-            default=1,
-            help='Maximum rows per exception argument (a shortened string is displayed). Default 1'
-            )
-    argparser.add_argument(
-                "--width",
-                dest="width",
-                type=int,
-                help="Defaults to active terminal's width (run 'echo $COLUMNS' to view)"
-                )
-    argparser.add_argument(
-            "--backend",
-            type=str,
-            choices=['openapi-spec-validator', ],
-            default='openapi-spec-validator',
-            help='Default: openapi-spec-validator'
-            )
-    argparser.add_argument(
-            "--working-dir",
-            type=str,
-            default='.',
-            help='Set script working directory'
-            )
-    
-    parsed_args = argparser.parse_args()
-    target = parsed_args.target
-    rows = parsed_args.rows
-    backend = parsed_args.backend
-    working_dir = parsed_args.working_dir
-    width = parsed_args.width
-    os.chdir(working_dir)
-
-    if not width:
-        width = 10 * (int(os.get_terminal_size()[0]) // 10) - 5  # round down
-    
     try:
         parser = ResolvingParser(
                 target,
@@ -94,7 +107,7 @@ def main():
                 backend=backend
                 )
     except Exception as e:
-        print_error_args(e.args, width=width, rows=rows)
+        print_error_args(e.args)
     else:
         print(f'Valid (no errors): {target}')
 
